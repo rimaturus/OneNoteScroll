0d338e892625dc593cbce98ec369498a28cc56f9
Rimaturus <41435912+rimaturus@users.noreply.github.com>
2022-03-03 17:22:46 +0100

    Update OneNoteScroll.py

--- OneNoteScroll.py
+++ OneNoteScroll.py
@@ -1,5 +1,18 @@
 from PIL import Image
-import glob, os
+from pdf2image import convert_from_path
+import os, glob
+
+for file in glob.glob("*.pdf"):
+    pages = convert_from_path(file, 144)
+    file_name = file.split('.')[0]
+    count = 0
+    for page in pages:
+        image_name = file_name + '-' + str(count) + ".png"
+        page.save(image_name, 'PNG')
+        count += 1
+        
+#input = "MES_20210303.pdf"
+#output pdf2png =  "MES_20210303-1.png" "MES_20210303-2.png" ...
 
 #---limiti crop---
 # misurati su file convertito con DPI = 144
@@ -33,11 +46,12 @@ for file in glob.glob("*.png"):
         for file in glob.glob("*.png"):
             if(file.split('-')[0] in lista):
                 if(count == 0):
-                    outfile = file.replace("-001.png","_long.png")
+                    outfile = file.replace("-0.png","_long.png")
                 im = Image.open(file)    
                 im1 = im.crop((left, top, right, bottom))
                 new_im.paste(im1, (0, (bottom-top)*count))
                 count += 1
+                os.remove(file)
         #new_im.show()
         new_im.save(outfile, "PNG")
         #-----------------------------------------
@@ -56,12 +70,17 @@ new_im = Image.new('RGB', (width, height))
 for file in glob.glob("*.png"):
     if(file.split('-')[0] in lista):
         if(count == 0):
-            outfile = file.replace("-001.png","_long.png")
+            outfile = file.replace("-0.png","_long.png")
         im = Image.open(file)    
         im1 = im.crop((left, top, right, bottom))
         new_im.paste(im1, (0, (bottom-top)*count))
         count += 1
+        os.remove(file)
 #new_im.show()
 new_im.save(outfile, "PNG")
 #-----------------------------------------
 
+for file in glob.glob("*.png"):
+    image = Image.open(file)
+    image2pdf = image.convert('RGB')
+    image2pdf.save(file.replace(".png",".pdf"))
